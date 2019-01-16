605f2a6a093a690884912950d1a7bd3b3f7e10a9
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-01-16 07:06:23 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,43 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to '/posts'
+  end
 
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    Post.create(params[:post])
+
+    redirect to '/posts'
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    @post = Post.find(params[:id])
+    @post.update(params[:post])
+    redirect to "/posts/#{params[:id]}"
+  end
+
+  delete '/posts/:id' do
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect to '/posts'
   end
 end
