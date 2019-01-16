a1df616d8ad222ec7389e1f3ea85e3f2cb8f03a8
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-01-16 06:25:19 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,38 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to '/posts'
+  end
 
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    Post.create(params[:post])
+
+    redirect to '/posts'
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/post/:id' do
+    @post = Post.find(params[:id])
+    erb :index
+  end
+
+  get '/posts/:id/edit' do
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    redirect to '/posts/:id'
+  end
+
+  delete '/posts/:id' do
+    redirect '/posts'
   end
 end
